68167ae7126503ded3e04bf1e3944b5ae08568e3
Rick Trager <abrennenburgskiy@gmail.com>
2024-08-04 05:34:18 +0300

    Started OOP

--- algebra01.lua
+++ algebra01.lua
@@ -1,11 +1,55 @@
-expression = "5x^2 + 3xy^3 - 10x + 2y"
--- 5x^2, 3xy^3, -10x, 2y
-expression = "2x^3+5x^2-7x+19"
+Monomial = {}
+Monomial.__index = Monomial
 
-function removeSpaces(str)
-  result = ""
-  for i = 1, #str do
-    local c = str:sub(i, i)
+Polynomial = {}
+Polynomial.__index = Polynomial
+
+---@param coefficient integer
+---@param variable string
+---@param power integer
+function Monomial:new(coefficient, variable, power)
+  local mon = {
+    coefficient = coefficient,
+    variable    = variable,
+    power       = power
+  }
+  return setmetatable(mon, Monomial)
+end
+
+function Monomial:toString()
+  return tostring(self.coefficient)..self.variable.."^"..tostring(self.power)
+end
+
+
+function Monomial:add(other)
+  if self.variable == other.variable then
+    if self.power == other.power then
+      return Monomial:new(self.coefficient + other.coefficient, self.variable, self.power)
+    end
+  end
+  return Polynomial:new({self, other})
+end
+
+function Polynomial:new(listOfMonomials)
+  local poly = {}
+  for i = 1, #listOfMonomials do
+    poly[#poly+1] = listOfMonomials[i]
+  end
+  return setmetatable(poly, Polynomial)
+end
+
+
+
+
+
+
+
+
+
+function removeSpaces(polynomial)
+  local result = ""
+  for i = 1, #polynomial do
+    local c = polynomial:sub(i, i)
     if c ~= " " then
       result = result .. c
     end
@@ -13,7 +57,7 @@ function removeSpaces(str)
   return result
 end
 
-function parse(expr)
+function getListOfMonomials(expr)
   local result   = {}
   local monomial = ""
   expr = removeSpaces(expr)
@@ -22,25 +66,20 @@ function parse(expr)
     if c == "+" then
       result[#result+1] = monomial
       monomial = ""
-      goto continue 
+      goto continue
     elseif c == "-" then
       result[#result+1] = monomial
       monomial = "-"
-      goto continue 
+      goto continue
     end
     monomial = monomial .. c
     ::continue::
   end
   result[#result+1] = monomial
-  
   return result
 end
 
-function analyze(monomial) -- -> coefficient, variables, powers
-  
-end
-
-mon1 = "2x^5"
-
-result = analyze(mon1)
-
+-- tests
+first = Monomial:new(5, "x", 3)
+second = Monomial:new(1, "x", 2)
+print(first:toString())
